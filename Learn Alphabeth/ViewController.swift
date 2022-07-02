a11a3c0352809ad0e8e1b9c540a748bc25873818
Mehmet Arisu <108470941+MehmetRasitArisu@users.noreply.github.com>
2022-07-02 23:50:57 +0300

    Initial Commit

---
+++ Learn Alphabeth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Learn Alphabeth
+//
+//  Created by Rashit on 02/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
